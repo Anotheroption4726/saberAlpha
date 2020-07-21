672c37fc83682a18db1af02b5ac7484a21d5e64f
Adrien <napalm_ground@hotmail.com>
2020-07-21 23:36:16 +0200

    space key up & down

--- Assets/Scripts/CharacterScript.cs
+++ Assets/Scripts/CharacterScript.cs
@@ -4,15 +4,16 @@ using UnityEngine;
 
 public class CharacterScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown("space"))
+        {
+            print("space key was pressed");
+        }
+
+        if (Input.GetKeyUp("space"))
+        {
+            print("Space key was released");
+        }
     }
 }
