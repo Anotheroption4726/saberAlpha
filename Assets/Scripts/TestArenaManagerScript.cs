ee6913e958d2ef1bd225fd72d8236c8ddbc0a5da
Adrien <napalm_ground@hotmail.com>
2020-07-23 16:01:23 +0200

    TimeScale management

---
+++ Assets/Scripts/TestArenaManagerScript.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestArenaManagerScript : MonoBehaviour
+{
+    [SerializeField] private float timeScale;
+
+    void Awake()
+    {
+        Time.timeScale = timeScale;
+    }
+}
