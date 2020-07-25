b90fa310a25c55cb1f4f0df64984c8ca6685daa1
Adrien <napalm_ground@hotmail.com>
2020-07-26 01:44:07 +0200

    gravity optimized

--- Assets/Scripts/TestArenaManagerScript.cs
+++ Assets/Scripts/TestArenaManagerScript.cs
@@ -6,10 +6,12 @@ using UnityEngine.SceneManagement;
 public class TestArenaManagerScript : MonoBehaviour
 {
     [SerializeField] private float timeScale;
+    [SerializeField] private float gravityScale;    //  4.75f
 
     void Awake()
     {
         Time.timeScale = timeScale;
+        Physics2D.gravity = new Vector2(Physics2D.gravity.x, Physics2D.gravity.y * gravityScale);
     }
 
     private void Update()
