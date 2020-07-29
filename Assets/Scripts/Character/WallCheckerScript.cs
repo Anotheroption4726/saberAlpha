fc3a3b45122ffc637f285687e5f646871300668d
Adrien <napalm_ground@hotmail.com>
2020-07-29 23:33:07 +0200

    WallCheckerScript

---
+++ Assets/Scripts/Character/WallCheckerScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallCheckerScript : MonoBehaviour
+{
+    private bool isTouchingWall;
+    [SerializeField] private LayerMask groundLayerMask;
+
+    public bool GetIsTouchingWall()
+    {
+        return isTouchingWall;
+    }
+
+    private void OnTriggerStay2D(Collider2D arg_collider)
+    {
+        if (!isTouchingWall && arg_collider != null & ((1 << arg_collider.gameObject.layer) & groundLayerMask) != 0)
+        {
+            isTouchingWall = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D arg_collider)
+    {
+        if (isTouchingWall && arg_collider != null & ((1 << arg_collider.gameObject.layer) & groundLayerMask) != 0)
+        {
+            isTouchingWall = false;
+        }
+    }
+}
