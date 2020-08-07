c0812ecd2ba8dcdcc21836c73407a10b886f0eb2
Adrien <napalm_ground@hotmail.com>
2020-08-07 19:23:38 +0200

    Character class setup

---
+++ Assets/Scripts/Character.cs
@@ -0,0 +1,41 @@
+﻿public class Character
+{
+    //  Run Parameters
+    private float fixed_run_movementSpeed = 40;
+    private float timer_run_stopSlideTime = 0.13f;
+
+    //  Idle Jump Parameters
+    private float impulse_idleJump_verticalForce = 1200;
+    private float fixed_idleJump_movementSpeed = 10;
+
+    // Forward Jump Parameters
+    private float impulse_forwardJump_horizontalForce = 250;
+    private float impulse_forwardJump_stopSlideForce = 1500;
+    private float ratio_forwardJump_horizontalAirDrag = 0.97f;   //0.997f
+
+    //  Fall MaxSpeed Parameters
+    private float threshold_fallMaxSpeed_velocityValue = 60;
+
+    //  On the Ground Parameters
+    private float timer_onTheGround_duration = 2;
+    private float timer_onTheGround_StandUpTime = 0.5f;
+    private bool trigger_onTheGround_isOntheGround = false;
+
+    //  Crawl Parameters
+    private float fixed_crawl_movementSpeed = 10;
+
+    //  Run Slide Parameters
+    private float impulse_runSlide_horizontalForce = 2500;
+    private float timer_runSlide_startTime = 0.75f;
+    private float timer_runSlide_duration = 0.25f;
+    private bool trigger_runSlide_canRunSlide = false;
+
+    //  WallSlide Parameters
+    private float ratio_wallSlide_holdGravity = 0.125f;
+
+    //  WallJump Parameters
+    private float impulse_wallJump_verticalForce = 800;
+    private float impulse_wallJump_horizontalForce = 1500;
+    private float timer_wallJump_restrainDuration = 0.25f;
+    private bool trigger_wallJump_hasWallJumped = false;
+}
