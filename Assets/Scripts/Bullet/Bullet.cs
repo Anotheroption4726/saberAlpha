3804a9b0df733555813efbb10a7b2c6bbc2f62b4
Adrien <napalm_ground@hotmail.com>
2020-10-10 12:01:37 +0200

    Bullet animation setup

---
+++ Assets/Scripts/Bullet/Bullet.cs
@@ -0,0 +1,14 @@
+﻿public class Bullet
+{
+    private float bulletAppearAnimationTime;
+
+    public Bullet(float arg_bulletAppearAnimationTime = 4f)
+    {
+        bulletAppearAnimationTime = arg_bulletAppearAnimationTime;
+    }
+
+    public float GetBulletAppearAnimationTime()
+    {
+        return bulletAppearAnimationTime;
+    }
+}
---
+++ Assets/Scripts/Bullet/BulletScript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletScript : MonoBehaviour
+{
+    private Bullet bullet = new Bullet();
+    private BulletAnimStateEnum animState = BulletAnimStateEnum.Bullet_Appear;
+    [SerializeField] private Animator animator;
+
+    // Start
+    void Start()
+    {
+        SetAnimation(BulletAnimStateEnum.Bullet_Appear);
+        StartCoroutine(EndAnimationCoroutine(bullet.GetBulletAppearAnimationTime(), BulletAnimStateEnum.Bullet_Travel));
+    }
+
+    //  Coroutines
+    IEnumerator EndAnimationCoroutine(float arg_time, BulletAnimStateEnum arg_animState)
+    {
+        yield return new WaitForSeconds(arg_time);
+        SetAnimation(arg_animState);
+    }
+
+    //  Animation functions
+    private void SetAnimation(BulletAnimStateEnum arg_bulletAnimStateEnum)
+    {
+        if (animState == arg_bulletAnimStateEnum)
+        {
+            return;
+        }
+
+        animator.Play(arg_bulletAnimStateEnum.ToString());
+        animState = arg_bulletAnimStateEnum;
+    }
+}
