0d791be4c8ac4f2e1e1c1e2b40a5ddf887cc1407
Adrien <napalm_ground@hotmail.com>
2020-10-10 12:13:47 +0200

    Bullet basic movement

--- Assets/Scripts/Bullet/Bullet.cs
+++ Assets/Scripts/Bullet/Bullet.cs
@@ -1,10 +1,17 @@
 ﻿public class Bullet
 {
+    private int bulletMovementSpeed;
     private float bulletAppearAnimationTime;
 
-    public Bullet(float arg_bulletAppearAnimationTime = 4f)
+    public Bullet(int arg_bulletMovementSpeed = 500, float arg_bulletAppearAnimationTime = 0.5f)
     {
+        bulletMovementSpeed = arg_bulletMovementSpeed;
         bulletAppearAnimationTime = arg_bulletAppearAnimationTime;
+    }
+
+    public float GetBulletMovementSpeed()
+    {
+        return bulletMovementSpeed;
     }
 
     public float GetBulletAppearAnimationTime()
--- Assets/Scripts/Bullet/BulletScript.cs
+++ Assets/Scripts/Bullet/BulletScript.cs
@@ -7,12 +7,20 @@ public class BulletScript : MonoBehaviour
     private Bullet bullet = new Bullet();
     private BulletAnimStateEnum animState = BulletAnimStateEnum.Bullet_Appear;
     [SerializeField] private Animator animator;
+    private Rigidbody2D rigidbody;
+
+    private void Awake()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+    }
 
     // Start
     void Start()
     {
         SetAnimation(BulletAnimStateEnum.Bullet_Appear);
         StartCoroutine(EndAnimationCoroutine(bullet.GetBulletAppearAnimationTime(), BulletAnimStateEnum.Bullet_Travel));
+
+        rigidbody.AddForce(new Vector2(bullet.GetBulletMovementSpeed(), 0));
     }
 
     //  Coroutines
